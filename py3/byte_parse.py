096cc6853ea468908c44761b9dd81100996320cc
yamsbot <devonbonderenka@gmail.com>
2024-08-30 14:16:25 -0400

    lol

--- py3/byte_parse.py
+++ py3/byte_parse.py
@@ -1,29 +1,28 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import sys
 
-# DEFINES
 bad_char = False
 bytestr = ""
 count = 0
 bad = 0
 
-# COLORS
 red = "\033[91m"
 grn = "\033[92m"
 reg = "\033[00m"
 
-# MAIN
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         sys.exit()
 
     if len(sys.argv) == 3:
         bad_char = True
+        bad_chars = sys.argv[2].split(",")
+        print("bad", bad_chars)
 
     with open(sys.argv[1], "rb") as f:
         while(byte := f.read(1)):
             count += 1
-            if bad_char == True and byte.hex() == sys.argv[2]:
+            if bad_char and byte.hex() in bad_chars:
                 bytestr += red + "\\x" + byte.hex() + reg
                 bad += 1
             else:
